20fa3f008d227fa62f549cbb000c860619fb5f6a
Gabriel Cardoso Martins <45346351841@info.local>
2019-02-18 11:28:21 -0300

    first commit

---
+++ Api.Personagens/Api.Personagens/Domains/PersonagensDomain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Personagens.Domains
+{
+    public class PersonagensDomain
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(maximumLength:150,MinimumLength =3 , ErrorMessage = "Numero de caracteres inválido")]
+        public string Personagens { get; set; }
+
+        [Required]
+        public string Lancamento { get; set; }
+    }
+}
